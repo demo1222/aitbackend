3d87a5a38e4d55dcc40d3dd69d92502ae18c0ab3
demo1222 <Democtras05@gmail.com>
2025-05-04 15:29:10 +0600

    init

---
+++ src/main/java/com/backend/aitbackend/AitbackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.aitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/aitbackend/AitbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.aitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
